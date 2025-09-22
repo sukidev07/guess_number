ac90e794490d9a63869b08f507eff5432df3d009
suki <sukidev07@gmail.com>
2025-09-21 21:55:24 -0400

    While loop with clear count down tries and a break with updated notes and refactoring to improve the code to be DRY

--- main.py
+++ main.py
@@ -1,29 +1,43 @@
 # This is our first Python program!
 # The print() function displays text on the screen.
-print("Welcome to the Guess the Number Game!")
-print("I'm thinking of a number between 1 and 10.")
+
+# Moved variables higher in the stack to validate the print statements
 
 # We create a variable to store our secret number.
 secret_number = 5
 
-# Get the user's guess and convert it to an integer.
-user_guess = int(input("Enter your guess: "))
+# guess_count defines the total number of guesses allowed
+guess_count = 3
 
-# Creating a loop for number of guesses to get correct
-while user_guess != secret_number:
-    # restructure of the code for an lower or hire stance
-    # we changed from an if elif elif to a if else in a while loop
-    if user_guess < secret_number:
+print("Welcome to the Guess the Number Game!")
+print("I'm thinking of a number between 1 and 10.")
+print(f"You have {guess_count} to guess the number.")
+
+# create a number guessing loop
+# guess if not equal to value will loop until max numbers tried or correct guess
+while guess_count != 0:
+    
+    # Guess moved to validate guess attempts
+    user_guess = int(input("Enter your guess: "))
+    # Adding the guess reduction count at the top of the code to keep it DRY
+    guess_count -= 1
+
+    if user_guess == secret_number:
+        # This block runs if the guess is correct.
+        # We moved the correct statement inside and moved the failed message outside
+        # Added break to clean clear out of the loop onces reached
+        print(f"Congratulations! You guessed the number {secret_number}!")
+        print(f"total guesses reamining {guess_count}/3")
+        break  # Exit the loop if the guess is correct 
+    elif user_guess < secret_number:
         # This block runs if the guess is too low.
         print("Too low! Try guessing a little higher.")
+        print(f"{guess_count} guesses left")
     else:
         # This block runs if the guess is too high.
         print("Too high! Try guessing a little lower.")
+        print(f"{guess_count} guesses left")
     
-    # This is fitting inline with the if else with the prints for too high too low are indented
-    # Create the repeat value to update the user_guess value
-    user_guess = int(input("Try again: "))
-
-# This block runs ONLY if the guess is correct.
-# This print is outside the loop and enacts if the value is correct (ie: user_guess == secret_number)
-print("You got it! Congratulations!")
+# adding the failed outside while statement to have the counter catch if 
+if user_guess != secret_number:
+    print(f"Sorry, you have {guess_count} guesses left. The number was {secret_number}.")
