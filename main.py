f5d83cf20a00ee18602c882c6d9a779dd11227c8
suki <sukidev07@gmail.com>
2025-09-21 20:12:39 -0400

    added the while loop if value to not match keep asking and getting new prompt to change the user_guess variable

--- main.py
+++ main.py
@@ -9,16 +9,21 @@ secret_number = 5
 # Get the user's guess and convert it to an integer.
 user_guess = int(input("Enter your guess: "))
 
-# Compare the guess to the secret number using an if/else statement.
-if user_guess == secret_number:
-    # This block runs ONLY if the guess is correct.
-    print("You got it! Congratulations!")
-elif user_guess < secret_number:
-    # This block runs if the guess is too low.
-    print("Too low! Try guessing a little higher.")
-elif user_guess > secret_number:
-    # This block runs if the guess is too high.
-    print("Too high! Try guessing a little lower.")
-else:
-    # This block runs ONLY if the guess is incorrect.
-    print("Sorry, that's not the correct number.")
+# Creating a loop for number of guesses to get correct
+while user_guess != secret_number:
+    # restructure of the code for an lower or hire stance
+    # we changed from an if elif elif to a if else in a while loop
+    if user_guess < secret_number:
+        # This block runs if the guess is too low.
+        print("Too low! Try guessing a little higher.")
+    else:
+        # This block runs if the guess is too high.
+        print("Too high! Try guessing a little lower.")
+    
+    # This is fitting inline with the if else with the prints for too high too low are indented
+    # Create the repeat value to update the user_guess value
+    user_guess = int(input("Try again: "))
+
+# This block runs ONLY if the guess is correct.
+# This print is outside the loop and enacts if the value is correct (ie: user_guess == secret_number)
+print("You got it! Congratulations!")
