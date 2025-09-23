ad63ff2c2e06a6e3cab59ffa8fa36da03fdf5370
suki <sukidev07@gmail.com>
2025-09-22 23:35:04 -0400

    working on auotmating and adding selection and need to improve error handling and values but next iteration

--- main.py
+++ main.py
@@ -4,19 +4,58 @@
 # Created to import generated_servret_number.py
 import ran_num_mod, player_num_guess
 
-# Moved to the top for ingestion (order of operation)
-# Adding in min_max range values for the guessing game
-min_range = 1
-max_range = 50
-guess_count = 3 # guess_count defines the total number of guesses allowed
+# Changing up the game require use input before playing.
+# We want to see what skill level, 1-10:(3) 1-20:(4) 1-100(5)
+print("# # # # # # # # # # # # # # # # # # # # #")
+print("# ------------------------------------- #")
+print("# Welcome to the Guess the Number Game! #")
+print("# ------------------------------------- #")
+print("#                                       #")
+print("#        Select your skill level        #")
+print("#                                       #")
+print("#     Level       Range       Guess     #")
+print("#     + + + + + + + + + + + + + + +     #")
+print("#     |1. |Easy:   1-10        3  |     #")
+print("#     |2. |Medium: 1-20        3  |     #") 
+print("#     |3. |Hard:   1-100       4  |     #") 
+print("#     |4. |Custom: x-y         z  |     #")
+print("#     + + + + + + + + + + + + + + +     #")
+print("#                                       #")
+print("# # # # # # # # # # # # # # # # # # # # #")
+print("")
+choice = input("Select:").lower
+
+## I need to add error handling somewhere around these parts might need to break out choice into a new mod
+
+# Define the levels:
+if choice == 1 or 'easy':
+    min_range = 1
+    max_range = 10
+    guess_count = 3
+elif choice == 2 or 'medium' or 'med':
+    min_range = 1
+    max_range = 20
+    guess_count = 3
+elif choice == 3 or 'hard':
+    min_range = 1
+    max_range = 100
+    guess_count = 4
+#elif choice == 4 or 'custom':
+#    min_range = user_min
+#    max_range = user_max
+#    guess_count = user_count
+else:
+    print("Invalid selection. Defaulting to easy mode.")
+    min_range = 1
+    max_range = 10
+    guess_count = 3
+    
+# Game should actually start here:
 
 # Moved variables higher in the stack to validate the print statements
 # We create a variable to store our secret number.
 # Adding min-max_range to be addeed in to validate these values for the secret guess and updating ran_num_mod
 secret_number = ran_num_mod.ran_num_mod(min_range, max_range)
-
-# The print() function displays text on the screen.
-print("Welcome to the Guess the Number Game!")
 print(f"I'm thinking of a number between {min_range} and {max_range}.")
 print(f"You have {guess_count} to guess the number.")
 
