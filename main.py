44edc59372557660a3b26f0878de3370063ba54d
suki <sukidev07@gmail.com>
2025-09-23 22:55:20 -0400

    Might be my final one the game works, it has a bunch of replable features and pretty decent error handling now. Reworked the NOTES, validated the variable changes made the CAPS for most (still not 100% sure why lower vs CAP for variables) If you go through the previous commits you can see some workflow changes and iterations on ideas, maybe usefull for folks.

--- main.py
+++ main.py
@@ -1,42 +1,59 @@
 # main.py
 
 # This is our first Python program!
-# Adding imports to the tool box.
-# Moved function to its own location
-# Created to import generated_servret_number.py
+# During the build we split out actual functions into their own modules
+# Used local imports to perform jobs, this was to train the skills and improve logical flow for myself
 import ran_num_mod, player_num_guess, num_lvl_select
 
-# Game should actually start here:
-# We need a level select, which sets the min,max range and guess count
-MIN_RANGE, MAX_RANGE, GUESS_COUNT = num_lvl_select.level_select()
+# Added a game loop so we can keep playing with a single run
+while True:
 
-# Moved variables higher in the stack to validate the print statements
-# We create a variable to store our secret number.
-# Adding min-MAX_RANGE to be addeed in to validate these values for the secret guess and updating ran_num_mod
-secret_number = ran_num_mod.ran_num_mod(MIN_RANGE, MAX_RANGE)
-print("")
-print(f"I'm thinking of a number between {MIN_RANGE} and {MAX_RANGE}.")
-print(f"You have {GUESS_COUNT} to guess the number.")
+    # Game should actually start here:
+    # Level Selector: Forces the import of the major game variables
+    MIN_RANGE, MAX_RANGE, GUESS_COUNT = num_lvl_select.level_select()
 
-# Breaking out into a new DRY method we need a method to get a response outside the while loop
-# Get the users first guess
-# need to pass through the MIN_RANGE and MAX_RANGE values to the player guess else it fails
-user_guess = player_num_guess.get_player_guess(MIN_RANGE, MAX_RANGE) 
-GUESS_COUNT -= 1 # Still need to redact a single guess the count down begins
+    # Module to randomly generate the number based on the min and max range provided
+    # Needs to pass the ran_num_mod(MIN_RANGE, MAX_RANGE) to the function: Else it don't work 
+    secret_number = ran_num_mod.ran_num_mod(MIN_RANGE, MAX_RANGE) 
 
-# Refactor the while loop - interesting my original thought now comes into play.
-# We now loop until based on two conditions being TRUE
-while user_guess != secret_number and GUESS_COUNT != 0:
-    if user_guess < secret_number:
-        print("Too low! Try guessing a little higher.")
+    # Lets open the game up with the computer asking you to do a thing: GUESS A NUMBER!
+    print("")
+    print(f"I'm thinking of a number between {MIN_RANGE} and {MAX_RANGE}.")
+    print(f"You have {GUESS_COUNT} to guess the number.")
+
+    # Player guess need to be gotten so we ask but created the function that lives in another file, much like the princess in another castle
+    # Needs to pass the get_player_guess(MIN_RANGE, MAX_RANGE) to the function: Else it don't work 
+    user_guess = player_num_guess.get_player_guess(MIN_RANGE, MAX_RANGE) 
+    GUESS_COUNT -= 1 # Still need to redact a single guess the count down begins (pretty sure its require daniel)
+
+    # Refactor the while loop - interesting my original thought now comes into play.
+    # We now loop until based on two conditions being TRUE
+    while user_guess != secret_number and GUESS_COUNT != 0:
+        if user_guess < secret_number:
+            print("Too low! Try guessing a little higher.")
+        else:
+            print("Too high! Try guessing a little lower.")
+
+        print(f"{GUESS_COUNT} guesses left")
+
+        # loops back to get value but reduction of the min/max_range
+        # Needs to pass the get_player_guess(MIN_RANGE, MAX_RANGE) to the function: Else it don't work 
+        user_guess = player_num_guess.get_player_guess(MIN_RANGE, MAX_RANGE)
+        GUESS_COUNT -= 1
+    
+    # Based win or lose conditions 
+    if user_guess == secret_number:
+        print(f"Congratulations! You guessed the correct number: {secret_number}!")
+        print(f"Total guesses remaining: {GUESS_COUNT + 1}")
     else:
-        print("Too high! Try guessing a little lower.")
+        # cleaning up some of the game play words 
+        print(f"Sorry, you have {GUESS_COUNT} guesses left.")
+        print(f"Game over. The number was {secret_number}.")
+    
+    # part of the replay logic; yes or y lets you continue else all other break
+    print("Do you want to play again?")
+    play_again = input("Enter: Yes to play again: ").lower()
+    if play_again != "yes" and play_again != "y": # continue the game or break to end
+        break
 
-    print(f"{GUESS_COUNT} guesses left")
 
-    user_guess = player_num_guess.get_player_guess(MIN_RANGE, MAX_RANGE)
-    GUESS_COUNT -= 1
-if user_guess == secret_number:
-    print(f"Congratulations! You guessed the number in {GUESS_COUNT + 1}/3 guesses. The secret number was {secret_number}!")
-else:
-     print(f"Sorry, you have {GUESS_COUNT} guesses left. The number was {secret_number}.")   
--- num_lvl_select.py
+++ num_lvl_select.py
@@ -5,21 +5,25 @@
 def get_custom_range():
     while True: # Loop forever until we get a valid range
         try:
+            # Try to get input and convert it to integers for min max and guess count
             MIN_RANGE = int(input("Enter the minimum number for the range: "))
             MAX_RANGE = int(input("Enter the maximum number for the range: "))
             GUESS_COUNT = int(input("Enter the number of guesses allowed: "))   
-
-            if MAX_RANGE > MIN_RANGE and GUESS_COUNT > int(0):
+            
+            # Validates MAX is not smaller than Min because that would break stuff
+            # Validates GUESS_COUNT is also greater than 0 because that is also strange and would screw up logic
+            if MAX_RANGE > MIN_RANGE and GUESS_COUNT > 0:
                 # If the range is logical, return the values and exit the function
                 return MIN_RANGE, MAX_RANGE, GUESS_COUNT
             else:
                 # If not, print an error and the loop will repeat
                 print("Error: The maximum number must be greater than the minimum.")
+                print("       The number of guesses must be greater than 0.")
         except ValueError:
             print("Invalid input. Please enter whole numbers only.")
 
 def level_select():
-    
+
     # Changing up the game require use input before playing.
     # We want to see what skill level, 1-10:(3) 1-20:(4) 1-100(5)
     print("")
@@ -42,15 +46,18 @@ def level_select():
     print("")
     
     while True: # Loop to ensure we get a valid choice
-        choice = input("Choose an option (1-4): ").lower()
-        if choice == int(1) or "easy":
+        choice = input("Choose an option (1-4): ").lower() # get the choice input then compare (string value)
+        # return MIN_RANGE,MAX_RANGE,GUESS_COUNT values if you get confused
+        if choice == "1" or choice == "easy":
             return 1, 10, 3
-        elif choice == int(2) or "medium":
+        elif choice == "2" or choice == "medium" or choice == "med":
             return 1, 20, 3
-        elif choice == int(3) or "hard":
+        elif choice == "3" or choice == "hard":
             return 1, 100, 4
-        elif choice == int(4) or "custom":
+        # This one is fun as it loops to the above function to get your own values (forced and validated)
+        elif choice == "4" or choice == "custom":
             MIN_RANGE, MAX_RANGE, GUESS_COUNT = get_custom_range()
             return MIN_RANGE, MAX_RANGE, GUESS_COUNT
+        # Check on the 'error' and just loops back if you can't type correctly
         else:
             print("Invalid choice. Please enter a number between 1 and 4.")
